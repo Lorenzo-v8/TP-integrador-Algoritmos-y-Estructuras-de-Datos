9fd62c24f82535ddb3a0ae1f5f1c8fcf76cf3fc4
Camipoggi <camipoggi28@gmail.com>
2026-09-06 16:23:33 -0300

    Create catalogo.py

---
+++ src/catalogo.py
@@ -0,0 +1,9 @@
+from src.dominio.cancion import Cancion
+
+CATALOGO = [
+    Cancion("Bohemian Rhapsody", "Queen", "A Night at the Opera", 1975, 355),
+    Cancion("Billie Jean", "Michael Jackson", "Thriller", 1982, 294),
+    Cancion("Imagine", "John Lennon", "Imagine", 1971, 183),
+    Cancion("Hotel California", "Eagles", "Hotel California", 1976, 391),
+    Cancion("Like a Rolling Stone", "Bob Dylan", "Highway 61 Revisited", 1965, 369),
+]
