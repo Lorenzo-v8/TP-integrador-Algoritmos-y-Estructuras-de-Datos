4fccbc61713be65dd691b8c249449d5b46d4f57d
Camipoggi <camipoggi28@gmail.com>
2026-09-16 22:27:44 -0300

    Delete src/catalogo.py

--- src/catalogo.py
+++
@@ -1,9 +0,0 @@
-from src.dominio.cancion import Cancion
-
-CATALOGO = [
-    Cancion("Baby Lover", "Ñengo Flow", "Real G4 Life Vol. 3", 2017, 194),
-    Cancion("Temporary Fix", "One Direction", "Take Me Home", 2012, 213),
-    Cancion("Tarea Fina", "Patricio Rey y sus Redonditos de Ricota", "La Mosca y la Sopa", 1991, 249),
-    Cancion("American Girls", "Harry Styles", "Kiss All the Time. Disco, Occasionally.", 2026, 210),
-    Cancion("La Incondicional", "Luis Miguel", "Busca una Mujer", 1988, 224),
-]
